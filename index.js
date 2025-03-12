ad92acf0eb4e5546fc1a416a3c4d973db27f117e
John <jjhhoward1738@gmail.com>
2025-03-12 22:11:35 +0300

    adds files

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const headquarters = 42;
+    return Math.abs(someValue - headquarters);
+  }
+  function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)*264
+  }
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination-start)*264
+
+  }
+  function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    const distance=Math.abs(destination-start)*264
+    if (distance<=400){
+      return 0
+    } else if(distance>=400 && distance<=2000){
+      return (distance-400)*0.02
+    } else if(distance>2000 && distance<=2500 ){
+      return 25
+    }else {
+      return 'cannot travel that far'
+    }
+  }
